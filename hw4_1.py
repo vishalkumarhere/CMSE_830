61766ed407f68780d1f4724f9c94d82de6977540
vishalkumarhere <118157458+vishalkumarhere@users.noreply.github.com>
2023-09-29 21:57:02 -0400

    Update hw4_1.py

--- hw4_1.py
+++ hw4_1.py
@@ -1,69 +1,54 @@
-# Import necessary libraries
 import streamlit as st
-import seaborn as sns
 import pandas as pd
-import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Load the "adult" dataset from Seaborn
-df_adult = sns.load_dataset("adult")
-
-# Set the title and description for the web app
-st.title("Exploring the Adult Dataset")
-st.write("This web app allows you to explore the 'adult' dataset from Seaborn.")
-
-# Sidebar for filtering options
-st.sidebar.header("Filter Data")
-age_range = st.sidebar.slider("Select Age Range", min_value=0, max_value=100, value=(0, 100))
-education_levels = st.sidebar.multiselect("Select Education Levels", df_adult["education"].unique())
-income_level = st.sidebar.selectbox("Select Income Level", df_adult["income"].unique())
-
-# Apply filters to the dataset
-filtered_df = df_adult[
-    (df_adult["age"] >= age_range[0]) & (df_adult["age"] <= age_range[1]) &
-    (df_adult["education"].isin(education_levels)) &
-    (df_adult["income"] == income_level)
+# Load the "adult" dataset from an online source
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
+column_names = [
+    "age", "workclass", "fnlwgt", "education", "education_num", "marital_status",
+    "occupation", "relationship", "race", "sex", "capital_gain", "capital_loss",
+    "hours_per_week", "native_country", "income"
 ]
+df_adult = pd.read_csv(url, header=None, names=column_names, sep=",\s*", engine="python")
 
-# Display filtered data
-st.subheader("Filtered Data")
-st.write(filtered_df)
+# Streamlit app
+st.title("Exploring the Adult Dataset")
 
-# Data Visualization
-st.header("Data Visualization")
+# Display dataset summary
+st.write("### Dataset Summary")
+st.write(df_adult.head())
 
-# Bar chart of education levels
-st.subheader("Education Levels Count")
-edu_counts = filtered_df["education"].value_counts()
-st.bar_chart(edu_counts)
+# Data exploration options
+st.sidebar.title("Data Exploration Options")
 
-# Histogram of ages
-st.subheader("Age Distribution")
-st.hist(filtered_df["age"], bins=20, edgecolor='k')
-plt.xlabel('Age')
-plt.ylabel('Count')
-st.pyplot()
+# Show basic statistics
+if st.sidebar.checkbox("Show Basic Statistics"):
+    st.write("### Basic Statistics")
+    st.write(df_adult.describe())
 
-# Box plot of hours per week by education
-st.subheader("Box Plot of Hours Per Week by Education")
-plt.figure(figsize=(10, 6))
-sns.boxplot(x="education", y="hours-per-week", data=filtered_df)
-plt.xticks(rotation=45)
-plt.xlabel('Education Level')
-plt.ylabel('Hours Per Week')
-st.pyplot()
+# Show a bar chart of education levels
+if st.sidebar.checkbox("Education Level Counts"):
+    st.write("### Education Level Counts")
+    education_counts = df_adult["education"].value_counts()
+    st.bar_chart(education_counts)
 
-# Summary Statistics
-st.header("Summary Statistics")
-st.write(filtered_df.describe())
+# Show a count of individuals by gender
+if st.sidebar.checkbox("Gender Distribution"):
+    st.write("### Gender Distribution")
+    gender_counts = df_adult["sex"].value_counts()
+    st.pie_chart(gender_counts)
 
-# Export filtered data to CSV
-if st.button("Export Filtered Data to CSV"):
-    filtered_df.to_csv("filtered_data.csv", index=False)
-    st.success("Data exported successfully!")
+# Show income distribution by education level
+if st.sidebar.checkbox("Income by Education"):
+    st.write("### Income Distribution by Education")
+    income_by_education = df_adult.groupby("education")["income"].value_counts().unstack().fillna(0)
+    st.bar_chart(income_by_education)
 
-# Footer
-st.sidebar.text("Built with Streamlit by Your Name")
+# Display a sample of the data
+if st.sidebar.checkbox("Show Sample Data"):
+    st.write("### Sample Data")
+    sample_size = st.sidebar.slider("Number of Rows", 1, len(df_adult), 10)
+    st.write(df_adult.sample(sample_size))
 
-# Run the app
-if __name__ == "__main__":
-    st.run()
+# You can add more data exploration options as needed
+
